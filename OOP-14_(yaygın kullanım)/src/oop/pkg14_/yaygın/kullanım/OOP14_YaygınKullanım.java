f1def327ed736f12b037678431a4273d44fba67d
ilker yılmaz <53461108+ilkerylmaz@users.noreply.github.com>
2021-10-23 21:30:28 +0300

    commit java oop-14

---
+++ OOP-14_(yaygın kullanım)/src/oop/pkg14_/yaygın/kullanım/OOP14_YaygınKullanım.java
@@ -0,0 +1,17 @@
+package oop.pkg14_.yaygın.kullanım;
+
+class Ogrenci {
+
+    int ogrenciNo;
+    String isim;
+}
+
+public class OOP14_YaygınKullanım {
+
+    public static void main(String[] args) {
+        Ogrenci nesne1 = new Ogrenci();
+        System.out.println(nesne1.ogrenciNo);
+        System.out.println(nesne1.isim);
+    }
+
+}
